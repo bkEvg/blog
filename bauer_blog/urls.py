7871f177db21875eeea90416fc0e06ecbe497129
bk_evg <bk.evg@mail.ru>
2021-09-18 23:55:34 +0700

    cdn added

--- bauer_blog/urls.py
+++ bauer_blog/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', include('robots.urls')),
     path('search/', include('search.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
