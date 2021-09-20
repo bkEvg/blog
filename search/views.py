7f7d7e537b830cb3abea4bd0e7326388a3ad4711
bk_evg <bk.evg@mail.ru>
2021-09-20 16:13:17 +0700

    search fixed

--- search/views.py
+++ search/views.py
@@ -5,45 +5,40 @@ from django.contrib.postgres.search import SearchVector
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
+
 def search(request):
-	try:
-		q = request.GET['q']
-		posts = Post.objects.annotate(
-			search=SearchVector('title', 'tags__name'),
-		).filter(search=q, status='published').order_by('-created')
-
-		# object_list = Post.objects.filter(status='published').order_by('-created')
-
-		paginator = Paginator(posts, 10)  # posts in each page
-		page = request.GET.get('page')
+	if request.method == 'GET':
 		try:
-		    posts = paginator.page(page)
-		except PageNotAnInteger:
-		    # If page is not an integer deliver the first page
-		    posts = paginator.page(1)
-		except EmptyPage:
-		    # If page is out of range deliver last page of results
-		    posts = paginator.page(paginator.num_pages)
-		return render(request,
-				'blog/post/list.html',
-				{
-					'page': page,
-					'posts': posts,
-					'q': q,
-					'search': True
-				}
-			)
-		# return render(request, 'search/search.html', {'posts':posts})
-	except KeyError:
-		posts = []
-		page = None
-		return render(request,
-				'blog/post/list.html',
-				{
-					'page': page,
-					'page_obj': posts
-				}
-			)
-	# results = BlogPost.objects.filter(
-	# 	Q(title__icontains=your_search_query) | Q(intro__icontains=your_search_query) | Q(content__icontains=your_search_query)
-	# )
+			q = request.GET['q']
+			lookups= Q(title__icontains=q) | Q(tags__name__icontains=q) | Q(author__first_name__icontains=q) | Q(author__last_name__icontains=q)
+			posts = Post.objects.filter(lookups)
+			paginator = Paginator(posts, 10)  # posts in each page
+			page = request.GET.get('page')
+			try:
+			    posts = paginator.page(page)
+			except PageNotAnInteger:
+			    # If page is not an integer deliver the first page
+			    posts = paginator.page(1)
+			except EmptyPage:
+			    # If page is out of range deliver last page of results
+			    posts = paginator.page(paginator.num_pages)
+			return render(request,
+					'blog/post/list.html',
+					{
+						'page': page,
+						'page_obj': posts,
+						'q': q,
+						'search': True
+					}
+				)
+			# return render(request, 'search/search.html', {'posts':posts})
+		except KeyError:
+			posts = []
+			page = None
+			return render(request,
+					'blog/post/list.html',
+					{
+						'page': page,
+						'page_obj': posts
+					}
+				)
