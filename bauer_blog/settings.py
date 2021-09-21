8a0995e3febcbfd61ba4fcc54e8a3cb132c8e050
bk_evg <bk.evg@mail.ru>
2021-09-21 18:10:43 +0700

    fix

--- bauer_blog/settings.py
+++ bauer_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECURE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bauerblog.herokuapp.com']
 
