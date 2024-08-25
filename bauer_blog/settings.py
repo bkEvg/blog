d47f33cbed6af75974b066df9b63ac5f6acda930
Bauer Kirill <bk.evg@mail.ru>
2024-08-25 22:16:11 +0700

    deleted secrets

--- bauer_blog/settings.py
+++ bauer_blog/settings.py
@@ -159,11 +159,9 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', 'AKIAXXJ2XMKTOVDBHX5D')
-AWS_SECRET_ACCESS_KEY = os.environ.get(
-    'AWS_SECRET_ACCESS_KEY', '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')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME',
-                                         'bauerblog')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 # optional: if smb upload the same name file don't overwrite it
 AWS_S3_FILE_OVERWRITE = False
