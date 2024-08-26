5362dff74ede59db6d6a123ac9c1d059e7d50b30
Bauer Kirill <bk.evg@mail.ru>
2024-08-26 18:53:13 +0700

    aptimized methods for post model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Count
-from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.urls import reverse
 
@@ -74,45 +73,35 @@ class Post(models.Model):
         return reverse('post_detail', args=[self.slug])
 
     def get_previous_post(self):
-        post = get_object_or_404(Post, slug=self.slug, status='published',
-                                 publish=self.publish)
-        post_tags_ids = post.tags.values_list('id', flat=True)
-        similar_posts = Post.objects.filter(tags__in=post_tags_ids,
-                                            status='published',
-                                            created__lt=self.created
-                                            ).exclude(id=post.id)
-        similar_post = similar_posts.annotate(
-            same_tags=Count('tags')).order_by('-same_tags', '-publish')
+        post_tags_ids = self.tags.values_list('id', flat=True)
+        similar_posts = (Post.objects.
+                         filter(tags__in=post_tags_ids, status='published',
+                                created__lt=self.creat).exclude(id=self.id)
+                         .annotate(same_tags=Count('tags'))
+                         .order_by('-same_tags', '-publish'))
 
-        if similar_post:
-            return similar_post
-        else:
-            post_ = Post.objects.filter(status='published',
-                                        created__lt=self.created)
-            if post_:
-                return post_
-            else:
-                pass
+        if not similar_posts.exists():
+            similar_posts = Post.objects.filter(
+                status='published',
+                created__lt=self.created
+            ).exclude(id=self.id).order_by('-publish')
+        return similar_posts.first()
 
     def get_next_post(self):
-        post = get_object_or_404(Post, slug=self.slug,
-                                 status='published', publish=self.publish)
-        post_tags_ids = post.tags.values_list('id', flat=True)
-        similar_posts = Post.objects.filter(tags__in=post_tags_ids,
-                                            status='published',
-                                            created__gt=self.created
-                                            ).exclude(id=post.id)
-        similar_post = similar_posts.annotate(
-            same_tags=Count('tags')).order_by('-same_tags', '-publish')
-        if similar_post:
-            return similar_post
-        else:
-            post_ = Post.objects.filter(status='published',
-                                        created__gt=self.created)
-            if post_:
-                return post_
-            else:
-                pass
+        post_tags_ids = self.tags.values_list('id', flat=True)
+        similar_posts = (Post.objects
+                         .filter(tags__in=post_tags_ids,
+                                 status='published',
+                                 created__gt=self.created)
+                         .exclude(id=self.id)
+                         .annotate(same_tags=Count('tags'))
+                         .order_by('-same_tags', '-publish'))
+        if not similar_posts.exists():
+            similar_posts = Post.objects.filter(
+                status='published',
+                created__gt=self.created
+            ).exclude(id=self.id).order_by('-publish')
+        return similar_posts.first()
 
 
 class Comment(models.Model):
@@ -136,7 +125,7 @@ class Comment(models.Model):
         verbose_name_plural = 'комментарии'
 
     def __str__(self):
-        return 'Comment by {} on {}'.format(self.name, self.post)
+        return 'Комментарий от {} на {}'.format(self.name, self.post.title)
 
 
 class UniqueView(models.Model):
@@ -149,4 +138,4 @@ class UniqueView(models.Model):
         verbose_name_plural = 'просмотры'
 
     def __str__(self):
-        return self.ip
+        return f"Просмотр от {self.ip} на {self.post.title}"
