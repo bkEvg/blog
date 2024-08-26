b3beb18d49adaa0867b1975f624b78ebffeccfe6
Bauer Kirill <bk.evg@mail.ru>
2024-08-26 19:22:56 +0700

    search logic have been simplified

--- search/views.py
+++ search/views.py
@@ -6,38 +6,27 @@ from blog.models import Post
 
 
 def search(request):
-    if request.method == 'GET':
-        try:
-            q = request.GET['q']
-            lookups = Q(title__icontains=q) | Q(tags__name__icontains=q) | Q(
-                author__first_name__icontains=q) | Q(
-                    author__last_name__icontains=q)
-            posts = Post.objects.filter(lookups)
-            paginator = Paginator(posts, 10)
-            page = request.GET.get('page')
-            try:
-                posts = paginator.page(page)
-            except PageNotAnInteger:
-                # If page is not an integer deliver the first page
-                posts = paginator.page(1)
-            except EmptyPage:
-                # If page is out of range return last page of results
-                posts = paginator.page(paginator.num_pages)
-            context = {
-                        'page': page,
-                        'page_obj': posts,
-                        'q': q,
-                        'search': True
-                    }
-            return render(request,
-                          'blog/post/list.html',
-                          context)
-        except KeyError:
-            posts = []
-            page = None
-            return render(request,
-                          'blog/post/list.html',
-                          {
-                              'page': page,
-                              'page_obj': posts
-                          })
+    q = request.GET.get('q', '')  # Получаем 'q' с пустой строкой по умолчанию
+    lookups = Q(title__icontains=q) | Q(tags__name__icontains=q) | Q(
+        author__first_name__icontains=q) | Q(author__last_name__icontains=q)
+
+    posts = Post.objects.filter(lookups)
+    paginator = Paginator(posts, 10)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, то берем первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если страница вне диапазона, берем последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+
+    context = {
+        'page': page,
+        'page_obj': posts,
+        'q': q,
+        'search': True
+    }
+
+    return render(request, 'blog/post/list.html', context)
